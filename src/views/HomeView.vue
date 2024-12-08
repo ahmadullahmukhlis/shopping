9960b88bc68950412bf17744e07257cbf741d72d
ahmadullahmukhlis <nasarimukhlis@gmail.com>
2024-12-08 20:06:43 +0430

    the problem of the home is fxed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+
 
 <template>
   <main>
-    <TheWelcome />
+    weclome to the main
   </main>
 </template>
